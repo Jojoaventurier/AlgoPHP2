11156e9539f006c6ef1590da2dcf0125df25215a
Geoffroy Maurutto <165882140+Jojoaventurier@users.noreply.github.com>
2024-04-13 14:52:49 +0200

    updt

--- exo15.php
+++ exo15.php
@@ -5,7 +5,7 @@
     adresse e-mail a le bon format.<br>
     Affichage :<br>
     L'adresse elan@elan-formation.fr est une adresse e-mail valide.<br>
-    L'adresse contact@]elan est une adresse mail invalide.
+    L'adresse contact@elan est une adresse mail invalide.
 
 </p>
 
