a24eddf2225b16d017f3fd1f06cd236990cf51e5
Geoffroy Maurutto <165882140+Jojoaventurier@users.noreply.github.com>
2024-04-18 09:08:31 +0200

    removed useless code

--- exo12.php
+++ exo12.php
@@ -11,11 +11,5 @@
 
 $tableauValeurs=array(true,"texte",10,25.369,array("valeur1","valeur2"));
 
-// function renderVarDump($array) {
-//     for ($i = 0; $i < count($array); $i++) {
-        var_dump($tableauValeurs);
-//         echo "<br>";
-//     }
-// }
 
-// renderVarDump($tableauValeurs);
+var_dump($tableauValeurs);
