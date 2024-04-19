336832a8e165ba4b52e7919963339cb0c2823597
Geoffroy Maurutto <165882140+Jojoaventurier@users.noreply.github.com>
2024-04-19 12:00:33 +0200

    changed writing of <br/>

--- exo14.php
+++ exo14.php
@@ -104,8 +104,8 @@ class VoitureElec extends Voiture {
 $v1 = new Voiture("Peugeot", "408");
 $ve1 = new VoitureElec("BMW", "I3", 100);
 
-echo $v1->getInfos(). "<br/>";
-echo $ve1->getInfos(). "<br/>";
+echo $v1->getInfos(). "<br>";
+echo $ve1->getInfos(). "<br>";
 
 /* FROM https://www.w3schools.com/PHP/php_oop_inheritance.asp
 
